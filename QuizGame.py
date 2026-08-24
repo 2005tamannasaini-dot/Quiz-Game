97f2ff333c63a6280646612a848145d60ed3bf64
2005tamannasaini-dot <2005tamannasaini@email.com>
2026-08-24 11:23:06 +0530

    Added quiz question

---
+++ QuizGame.py
@@ -0,0 +1,23 @@
+# Quiz Game Project
+
+questions = [
+            {"question" : "Python kis type ki language hai?",
+             "options" : ["Programming language", "Markup language", "Database", "Operating system"] ,
+             "answer"  : "Programming language"},
+
+            {"question" : "Python me function banane ke liye kaunsa keyword use hota hai?",
+             "options" : ["func", "def", "function", "define"],
+             "answer"  : "def"},
+
+             {"question" : "Python me list kaunse brackets me banti hai?",
+              "options" : ["()", "{}", "[]", "<>"],
+              "answer"  : "[]"},
+
+             {"question" : "10 + 5 kitna hota hai?",
+              "options" : ["20", "10", "25", "15"],
+              "answer"  : "15"},
+
+             {"question" : "Computer me RAM ka full form kya hai?",
+              "options" : ["Random Access Memory", "Read Access Memory","Rapid Access Machine", "Random Application Memory"],
+              "answer"  : "Random Access Memory"}
+]
